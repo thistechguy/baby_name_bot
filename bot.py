1b66c76c05649b6ce54f987e33d077b5c2d23ecd
thistechguy <86990020+thistechguy@users.noreply.github.com>
2022-04-26 07:42:36 -0400

    Update bot.py
    Sanitized connection information for security.

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import random
 
 
 client = tweepy.Client(
-    consumer_key="vhaEN7oPE8TJKcm4eLhtB6YdJ",
-    consumer_secret="vedXZsui47wPyadPyvDlAvizPTGc2ADZGvVDNJvh1uZz6mPrnO",
-    access_token="1511667960330469376-1hGsrv19CEQjEw2e2MbowkLXPEmkPg",
-    access_token_secret="yaF6O4XlymmyUHuAvmUjVEIkNxrvrXVIo16kb02U1vIBa"
+    consumer_key="",
+    consumer_secret="",
+    access_token="",
+    access_token_secret=""
 )
 
 selectBabyName = random.choice(baby_names)
